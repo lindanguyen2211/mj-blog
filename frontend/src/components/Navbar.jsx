08e45eee334ee5973ddb496f970b0e6fa00c7424
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2025-06-28 23:01:06 -0500

    [feat(main page)] add navigation bar content

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+        <ul>
+            <li><a href="#">articles</a></li>
+            <li><a href="#">webinars</a></li>
+            <li><a href="#">user manual</a></li>
+            <li><a href="#">4d blog</a></li>
+            <li><a href="#">tutorials</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
