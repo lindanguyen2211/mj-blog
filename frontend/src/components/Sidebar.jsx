eccb049303dcbdee8acbed36bb536514b7f3edb0
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2025-06-30 22:29:45 -0500

    [chore] updated filter list

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -10,28 +10,32 @@ const Sidebar = () => {
                 </div>
                 <hr />
                 <div className='input-row'>
-                    <input type='checkbox' name='prosthodontics' id='prosthodontics' value='prosthodontics' />
-                    <label htmlFor='prosthodontics'>Prosthodontics</label>
-                </div>
-                <div className='input-row'>
-                    <input type='checkbox' name='orthodontics' id='orthodontics' value='orthodontics' />
-                    <label htmlFor='orthodontics'>Orthodontics</label>
-                </div>
-                <div className='input-row'>
-                    <input type='checkbox' name='tmdtmj' id='tmdtmj' value='tmdtmj' />
-                    <label htmlFor='tmdtmj'>TMD/TMJ</label>
-                </div>
-                <div className='input-row'>
-                    <input type='checkbox' name='periodontics' id='periodontics' value='periodontics' />
-                    <label htmlFor='periodontics'>Periodontics</label>
-                </div>
-                <div className='input-row'>
-                    <input type='checkbox' name='oral' id='oral' value='oral' />
-                    <label htmlFor='oral'>Oral and Maxillofacial</label>
+                    <input type='checkbox' name='education' id='education' value='education' />
+                    <label htmlFor='education'>Education</label>
                 </div>
                 <div className='input-row'>
                     <input type='checkbox' name='implantology' id='implantology' value='implantology' />
                     <label htmlFor='implantology'>Implantology</label>
+                </div>
+                <div className='input-row'>
+                    <input type='checkbox' name='Occlusodontology' id='Occlusodontology' value='Occlusodontology' />
+                    <label htmlFor='Occlusodontology'>Occlusodontology & TMD</label>
+                </div>
+                <div className='input-row'>
+                    <input type='checkbox' name='prosthodontic' id='prosthodontic' value='prosthodontic' />
+                    <label htmlFor='prosthodontic'>Prosthodontic</label>
+                </div>
+                <div className='input-row'>
+                    <input type='checkbox' name='splint' id='splint' value='splint' />
+                    <label htmlFor='splint'>Splint</label>
+                </div>
+                <div className='input-row'>
+                    <input type='checkbox' name='studies' id='studies' value='studies' />
+                    <label htmlFor='studies'>Studies</label>
+                </div>
+                <div className='input-row'>
+                    <input type='checkbox' name='technologies' id='technologies' value='technologies' />
+                    <label htmlFor='technologies'>Technologies</label>
                 </div>
             </form>
         </aside>
