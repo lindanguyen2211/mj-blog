2cd66858f62a004f536a33457c51b9d944e9e0cf
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2025-07-06 17:30:16 -0500

    feat(articles): add article tags

--- frontend/src/components/ArticleCard.jsx
+++ frontend/src/components/ArticleCard.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import placeholder from '../assets/banner.png'; // Update with your image path
 
 // const ArticleCard = ({ title, description, date, image }) => {
-const ArticleCard = ({ title, date }) => {
+const ArticleCard = ({ title, date,tags }) => {
   return (
     <div className='article-card'>
       {/* <img src={image || placeholder} alt={title} /> */}
       <h1>{title}</h1>
       <p className="date">{date}</p>
+      <div className='tag-container'>{tags.map((tag, index)=> (<p className="tag" key={index}>{tag}</p>))}</div>
     </div>
   );
 };
--- frontend/src/components/Articles.jsx
+++ frontend/src/components/Articles.jsx
@@ -11,12 +11,15 @@ const Articles = () => {
         "Accuracy of the maxillomandibular relationship at centric relation position recorded by using 3 different intraoral scanners and MODJAW",
       date: "June 27, 2025",
       pdf: "Accuracy-of-the-maxillomandibular-relationship-at-centric-relation-position-recorded-by-using-3-different-intraoral-scanners-and-MODJAW",
+      tags: ["modjaw","test"]
     },
     {
       id: 2,
       title: "Aesthetic & Function: the 4D solution clinical evaluation",
       date: "June 28, 2025",
       pdf: "Aesthetic & Function_the 4D solution clinical evaluation_EN",
+      tags: ["test"]
+
     },
     {
       id: 3,
@@ -24,24 +27,29 @@ const Articles = () => {
         "The FAS Aligner System and the treatment of temporomandibular disorder",
       date: "June 29, 2025",
       pdf: "aligners0223_P58_69_Martin_Final_NEW[295964]",
+      tags: ["modjaw"]
+
     },
     {
       id: 4,
       title: "An overview of the digital occlusion technologies",
       date: "June 30, 2025",
       pdf: "An overview of the digital occlusion technologies- Intraoral scanners, jaw tracking systems, and computerized occlusal analysis devices",
+      tags: ["modjaw","test"]
     },
     {
       id: 5,
       title: "Aesthetic & Function: A complete case with MODJAWf",
       date: "July 1, 2025",
       pdf: "ARTICLE_FREE_Aesthetic & Function_A complete case with MODJAW_EN",
+      tags: ["modjaw","test"]
     },
     {
       id: 6,
       title: "Full Digital Workflow for TMDs Management A Case Series",
       date: "July 2, 2025",
       pdf: "ARTICLE_FREE_Full-DigitalWorkflow for TMDs Management A Case Series - Dr Simona Tecco healthcare-11-00790",
+      tags: ["modjaw","test"]
     },
     {
       id: 7,
@@ -49,6 +57,7 @@ const Articles = () => {
         "MANAGE THE DYNAMIC OCCLUSION OF IMPLANT RESTORATIONS IN A DIGITAL WORKFLOW",
       date: "July 2, 2025",
       pdf: "ARTICLE_NOT FREE_MANAGE THE DYNAMIC OCCLUSION OF IMPLANT RESTORATIONS IN A DIGITAL WORKFLOW",
+      tags: ["modjaw","test"]
     },
     {
       id: 8,
@@ -56,12 +65,14 @@ const Articles = () => {
         "Modjaw in daily orthodontic practice: about a case treated with FAS aligners",
       date: "July 2, 2025",
       pdf: "Dr Domingo Martin_ortodoncia_actual_75-26-38",
+      tags: ["modjaw","test"]
     },
     {
       id: 9,
       title: "Full Digital rehabilitation in TMD patient Baldassarre",
       date: "July 2, 2025",
       pdf: "Full Digital rehabilitation in TMD patient Baldassarre",
+      tags: ["modjaw","test","digital"]
     },
     {
       id: 10,
@@ -69,12 +80,14 @@ const Articles = () => {
         "Fully digital workflow for the fabrication of occlusal stabilization splints",
       date: "July 2, 2025",
       pdf: "Fully digital workflow for the fabrication of occlusal stabilization splints",
+      tags: ["digital"]
     },
     {
       id: 11,
       title: "MODJAW - Ortho Case",
       date: "July 2, 2025",
       pdf: "MODJAW - Ortho Case",
+      tags: ["ortho"]
     },
     {
       id: 12,
@@ -82,6 +95,7 @@ const Articles = () => {
         "Thesis US Air Force: Mandibular Movement in 4D Accuracy of a Digital Mandibular Movement",
       date: "July 2, 2025",
       pdf: "Thesis_US_Air_Force_Mandibular Movement in 4D Accuracy of a Digital Mandibular Movement",
+      tags: ["4d"]
     },
   ];
 
@@ -89,9 +103,6 @@ const Articles = () => {
     <div className="articles-container">
       <h2>Latest Articles</h2>
       <div className="articles-grid">
-        <Link to="/articles" className="back-button">
-          <FaArrowLeft /> Back to Articles
-        </Link>
         {articles.map((article) => (
           <Link
             to={`/pdf/${article.pdf}`}
@@ -102,6 +113,7 @@ const Articles = () => {
               key={article.id}
               title={article.title}
               date={article.date}
+              tags={article.tags}
             />
           </Link>
         ))}
