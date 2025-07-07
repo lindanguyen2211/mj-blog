d27b8c3e254deb599824fd3e334d4fafb53d4561
Sherline Au <17630357+sherlineau@users.noreply.github.com>
2025-07-06 21:03:11 -0500

    fix- redirect 4d link to current blog

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <li><NavLink to="/articles" className={({ isActive }) => isActive ? 'active-link' : ''}>Articles</NavLink></li>
         <li><NavLink to="/webinars" className={({ isActive }) => isActive ? 'active-link' : ''}>Webinars</NavLink></li>
         <li><NavLink to="/pdf/UserManual-EN4.1" className={({ isActive }) => isActive ? 'active-link' : ''}>User Manual</NavLink></li>
-        <li><NavLink to="/4d-blog" className={({ isActive }) => isActive ? 'active-link' : ''}>4D Blog</NavLink></li>
+        <li><a href='https://modjaw.com/en/resources/4d-blog.html' target='_blank'>4D Blog</a></li>
         <li><NavLink to="/tutorials" className={({ isActive }) => isActive ? 'active-link' : ''}>Tutorials</NavLink></li>
         <li><NavLink to="/testimonials" className={({ isActive }) => isActive ? 'active-link' : ''}>Testimonials</NavLink></li>
         <li><NavLink to="/employee-resources" className={({ isActive }) => isActive ? 'active-link' : ''}>Employee Resources</NavLink></li>
